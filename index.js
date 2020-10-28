d880ca123fbfa989b57db6e4606627310bb5363d
Eduardo Costa <m4c0@users.noreply.github.com>
2020-10-28 13:09:24 +0000

    Grouping cmake steps

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const exec = require('@actions/exec');
 const io = require('@actions/io');
 
 async function run() {
-  core.info("Creating output folder");
-  await io.mkdirP('build');
+  await core.group("Creating output folder", async () => {
+    return await io.mkdirP('build');
+  });
 
   const config = core.getInput('config');
 
-  core.info("Configuring CMake");
-  await exec.exec(`cmake -DCMAKE_BUILD_TYPE=${config} -S . -B build`);
+  await core.group("Configuring CMake", async () => {
+    return await exec.exec(`cmake -DCMAKE_BUILD_TYPE=${config} -S . -B build`);
+  });
 
-  core.info("Building package using CMake");
-  await exec.exec(`cmake --build build --config ${config}`);
+  await core.group("Building package using CMake", async () => {
+    return await exec.exec(`cmake --build build --config ${config}`);
+  });
 
-  core.info("Testing package using CTest");
-  await exec.exec('ctest');
+  await core.group("Testing package using CTest", async () => {
+    return await exec.exec('ctest');
+  });
 
   core.setOutput('build_dir', 'build');
 }
 
-run();
+try {
+  run();
+} catch (error) {
+  core.setFailed(error.message);
+}
