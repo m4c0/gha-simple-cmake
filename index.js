6c913c208d9135fb97b27a04cf7f1f0940eb028f
Eduardo Costa <m4c0@users.noreply.github.com>
2020-10-28 12:57:25 +0000

    Wrapping async code

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const core = require('@actions/core');
 const exec = require('@actions/exec');
 const io = require('@actions/io');
 
-core.info("Creating output folder");
-await io.mkdirP('build');
+async function run() {
+  core.info("Creating output folder");
+  await io.mkdirP('build');
 
-const config = core.getInput('config');
+  const config = core.getInput('config');
 
-core.info("Configuring CMake");
-await exec.exec(`cmake -DCMAKE_BUILD_TYPE=${config} -S . -B build`);
+  core.info("Configuring CMake");
+  await exec.exec(`cmake -DCMAKE_BUILD_TYPE=${config} -S . -B build`);
 
-core.info("Building package using CMake");
-await exec.exec(`cmake --build build --config ${config}`);
+  core.info("Building package using CMake");
+  await exec.exec(`cmake --build build --config ${config}`);
 
-core.info("Testing package using CTest");
-await exec.exec('ctest');
+  core.info("Testing package using CTest");
+  await exec.exec('ctest');
 
-core.setOutput('build_dir', 'build');
+  core.setOutput('build_dir', 'build');
+}
+
+run();
