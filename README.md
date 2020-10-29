d7651cae25d4e5af2ce64971de3acb82b3ef21f3
Eduardo Costa <m4c0@users.noreply.github.com>
2020-10-29 14:28:41 +0000

    Added an option to set extra build flags

--- README.md
+++ README.md
@@ -10,9 +10,9 @@ cmake --build build --config $CONFIG
 
 ## Inputs
 
-### `config`
-
-**Required** CMake configuration to use (Debug, Release, MinSizeRel, and RelWithDebInfo)
+* `config` - **Required** CMake configuration to use (Debug, Release, MinSizeRel, and RelWithDebInfo)
+* `extra_configure_flags` - Flags to be sent while configuring CMake
+* `extra_build_flags` - Flags to be sent while using CMake to build
 
 ## Outputs
 
--- index.js
+++ index.js
@@ -9,13 +9,14 @@ async function run() {
 
   const config = core.getInput('config');
   const flags = core.getInput('extra_configure_flags') || '';
+  const build_flags = core.getInput('extra_build_flags') || '';
 
   await core.group("Configuring CMake", async () => {
     return await exec.exec(`cmake -DCMAKE_BUILD_TYPE=${config} -S . -B build ${flags}`);
   });
 
   await core.group("Building package using CMake", async () => {
-    return await exec.exec(`cmake --build build --config ${config}`);
+    return await exec.exec(`cmake --build build --config ${config} ${build_flags}`);
   });
 
   await core.group("Testing package using CTest", async () => {
